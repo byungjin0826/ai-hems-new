1a53e239f071bd707d5f257e3c9844d85f677527
byungjin0826 <byungjin0826@gmail.com>
2019-04-30 12:56:39 +0900

    update

--- utils.py
+++ utils.py
@@ -17,21 +17,21 @@ def get_table_from_db(sql):
                                                 port=3306, user='aihems', passwd='#cslee1234', db='aihems_service_db',
                                                 charset='utf8')
     df = pd.read_sql(sql, aihems_service_db_connect)
-    return (df)
+    return df
 
 
 # X 값을 변환하여 사용
-def window_stack(X, stepsize=10, lag=2):  # todo: 1. X가 여러개의 컬럼일 때도 동작할 수 있도록, 2. Lag 부분 추가
+def window_stack(x, step_size=10, lag=2):  # todo: 1. X가 여러개의 컬럼일 때도 동작할 수 있도록, 2. Lag 부분 추가
     """
     상태 판별 예측을 위한 입력 데이터 변환
-    :param X: 분 단위 전력 사용량
-    :param stepsize: Sliding window의 사이즈
+    :param x: 분 단위 전력 사용량
+    :param step_size: Sliding window 의 사이즈
     :param lag: 숫자만큼 지연 
     :return:
     """
-    X = [0] * (stepsize - 1) + X
-    X_transformed = [X[i - stepsize:i] for i in range(len(X) + 1) if i > stepsize - 1]
-    return (X_transformed)  #
+    x = [0] * (step_size - 1) + x
+    x_transformed = [x[i - step_size + lag:i + lag] for i in range(len(x) + 1) if i > step_size - 1]
+    return x_transformed  #
 
 
 def data_load():  # todo: Excel에서 데이터 불러오기, DB에 저장 완료 시 필요없음
@@ -42,7 +42,7 @@ def set_data():  # todo: 기존 함수 복사해서 붙여넣기
     return 0
 
 
-def split_x_y(df, x_col=[''], y_col=['']):  # todo: X와 Y 분리하기, 컬럼이 다수일 때도 가능하도록
+def split_x_y(df, x_col=['elec'], y_col=['status']):  # todo: X와 Y 분리하기, 컬럼이 다수일 때도 가능하도록, 명칭 다시 수정
     """
     DataFrame에서 X와 Y를 분리
     :param df: python DataFrame
@@ -113,193 +113,173 @@ def write_db(df, table_name='AH_USE_LOG_BYMINUTE_LABLED'):
     return (0)
 
 
-cols_dic = {
-    'ah_appliance': [
-        'appliance_no'
-        , 'appliance_type'
-        , 'appliance_name'
-        , 'maker'
-        , 'model_no'
-        , 'purchase_date'
-        , 'wait_power'
-        , 'use_power'
-        , 'flag_use_remocon'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_appliance_connect': [
-        'appliance_no'
-        , 'gateway_id'
-        , 'device_address'
-        , 'end_point'
-        , 'flag_delete'
-        , 'create_date'
-    ],
-    'ah_appliance_energy_history': [
-        'appliance_no'
-        , 'create_date'
-        , 'wait_energy'
-        , 'wait_minute'
-        , 'wait_power'
-        , 'use_energy'
-        , 'use_minute'
-        , 'use_power'
-    ],
-    'ah_appliance_history': [
-        'appliance_no'
-        , 'create_date'
-        , 'end_date'
-        , 'gateway_id'
-        , 'device_address'
-        , 'end_point'
-        , 'appliance_type'
-        , 'appliance_name'
-    ],
-    'ah_appliance_remocon_config': [
-        'appliance_no'
-        , 'device_address'
-        , 'end_point'
-        , 'maker'
-        , 'codeset'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_appliance_type': [
-        'appliance_type'
-        , 'appliance_type_name'
-        , 'appliance_type_descript'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_device': [
-        'device_address'
-        , 'end_point'
-        , 'device_type'
-        , 'device_name'
-        , 'control_reason'
-        , 'flag_use_ai'
-        , 'flag_use_metering'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_device_install': [
-        'gateway_id'
-        , 'device_address'
-        , 'end_point'
-        , 'flag_delete'
-        , 'create_date'
-    ],
-    'ah_gateway': [
-        'gateway_id'
-        , 'gateway_name'
-        , 'gateway_xmpp_id'
-        , 'gateway_mqtt_id'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_gateway_install': [
-        'house_no'
-        , 'gateway_id'
-        , 'flag_delete'
-        , 'create_date'
-    ],
-    'ah_house': [
-        'house_no'
-        , 'house_name'
-        , 'house_address'
-        , 'house_detail_address'
-        , 'meter_day'
-        , 'contract_type'
-        , 'ai_control_mode'
-        , 'flag_active_dr'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_house_member': [
-        'house_no'
-        , 'user_no'
-        , 'flag_delete'
-        , 'create_date'
-    ],
-    'ah_usage_daily_predict': [
-        'house_no'
-        , 'collected_date'
-        , 'use_energy'
-        , 'predict_use_energy'
-        , 'progressive_level'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_usage_monthly': [
-        'house_no'
-        , 'year'
-        , 'month'
-        , 'use_energy'
-        , 'predict_use_energy'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_usage_monthly_byappliance': [
-        'house_no'
-        , 'appliance_no'
-        , 'year'
-        , 'month'
-        , 'use_energy'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_usage_monthly_bydevice': [
-        'house_no'
-        , 'device_address'
-        , 'end_point'
-        , 'year'
-        , 'month'
-        , 'use_energy'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_user': [
-        'user_no'
-        , 'user_name'
-        , 'email'
-        , 'login_id'
-        , 'login_password'
-        , 'flag_delete'
-        , 'create_date'
-        , 'modify_date'
-    ],
-    'ah_use_log_byminute': [
-        'gateway_id'
-        , 'device_address'
-        , 'end_point'
-        , 'collected_date'
-        , 'collected_time'
-        , 'quality'
-        , 'onoff'
-        , 'energy'
-        , 'energy_diff'
-        , 'appliance_status'
-        , 'create_date'
-    ],
-    'ah_use_log_byminute_labled': [
-        'gateway_id'
-        , 'device_address'
-        , 'end_point'
-        , 'collected_date'
-        , 'collected_time'
-        , 'quality'
-        , 'onoff'
-        , 'energy'
-        , 'energy_diff'
-        , 'appliance_status'
-        , 'create_date'
-    ]
-}
+cols_dic = dict(ah_appliance=[
+    'appliance_no'
+    , 'appliance_type'
+    , 'appliance_name'
+    , 'maker'
+    , 'model_no'
+    , 'purchase_date'
+    , 'wait_power'
+    , 'use_power'
+    , 'flag_use_remocon'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_appliance_connect=[
+    'appliance_no'
+    , 'gateway_id'
+    , 'device_address'
+    , 'end_point'
+    , 'flag_delete'
+    , 'create_date'
+], ah_appliance_energy_history=[
+    'appliance_no'
+    , 'create_date'
+    , 'wait_energy'
+    , 'wait_minute'
+    , 'wait_power'
+    , 'use_energy'
+    , 'use_minute'
+    , 'use_power'
+], ah_appliance_history=[
+    'appliance_no'
+    , 'create_date'
+    , 'end_date'
+    , 'gateway_id'
+    , 'device_address'
+    , 'end_point'
+    , 'appliance_type'
+    , 'appliance_name'
+], ah_appliance_remocon_config=[
+    'appliance_no'
+    , 'device_address'
+    , 'end_point'
+    , 'maker'
+    , 'codeset'
+    , 'create_date'
+    , 'modify_date'
+], ah_appliance_type=[
+    'appliance_type'
+    , 'appliance_type_name'
+    , 'appliance_type_descript'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_device=[
+    'device_address'
+    , 'end_point'
+    , 'device_type'
+    , 'device_name'
+    , 'control_reason'
+    , 'flag_use_ai'
+    , 'flag_use_metering'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_device_install=[
+    'gateway_id'
+    , 'device_address'
+    , 'end_point'
+    , 'flag_delete'
+    , 'create_date'
+], ah_gateway=[
+    'gateway_id'
+    , 'gateway_name'
+    , 'gateway_xmpp_id'
+    , 'gateway_mqtt_id'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_gateway_install=[
+    'house_no'
+    , 'gateway_id'
+    , 'flag_delete'
+    , 'create_date'
+], ah_house=[
+    'house_no'
+    , 'house_name'
+    , 'house_address'
+    , 'house_detail_address'
+    , 'meter_day'
+    , 'contract_type'
+    , 'ai_control_mode'
+    , 'flag_active_dr'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_house_member=[
+    'house_no'
+    , 'user_no'
+    , 'flag_delete'
+    , 'create_date'
+], ah_usage_daily_predict=[
+    'house_no'
+    , 'collected_date'
+    , 'use_energy'
+    , 'predict_use_energy'
+    , 'progressive_level'
+    , 'create_date'
+    , 'modify_date'
+], ah_usage_monthly=[
+    'house_no'
+    , 'year'
+    , 'month'
+    , 'use_energy'
+    , 'predict_use_energy'
+    , 'create_date'
+    , 'modify_date'
+], ah_usage_monthly_byappliance=[
+    'house_no'
+    , 'appliance_no'
+    , 'year'
+    , 'month'
+    , 'use_energy'
+    , 'create_date'
+    , 'modify_date'
+], ah_usage_monthly_bydevice=[
+    'house_no'
+    , 'device_address'
+    , 'end_point'
+    , 'year'
+    , 'month'
+    , 'use_energy'
+    , 'create_date'
+    , 'modify_date'
+], ah_user=[
+    'user_no'
+    , 'user_name'
+    , 'email'
+    , 'login_id'
+    , 'login_password'
+    , 'flag_delete'
+    , 'create_date'
+    , 'modify_date'
+], ah_use_log_byminute=[
+    'gateway_id'
+    , 'device_address'
+    , 'end_point'
+    , 'collected_date'
+    , 'collected_time'
+    , 'quality'
+    , 'onoff'
+    , 'energy'
+    , 'energy_diff'
+    , 'appliance_status'
+    , 'create_date'
+], ah_use_log_byminute_labled=[
+    'gateway_id'
+    , 'device_address'
+    , 'end_point'
+    , 'collected_date'
+    , 'collected_time'
+    , 'quality'
+    , 'onoff'
+    , 'energy'
+    , 'energy_diff'
+    , 'appliance_status'
+    , 'create_date'
+])
 
 classifications = {
     'random forest': [
