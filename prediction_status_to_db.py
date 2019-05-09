c9844af3fe5182584ae08c077e17f0125340f0de
CHAEUNPARK <codjs0910@naver.com>
2019-05-09 17:30:50 +0900

    실증세대 4월 추가

--- prediction_status_to_db.py
+++ prediction_status_to_db.py
@@ -5,3 +5,7 @@ SELECT REPLACE(CURRENT_DATE()-INTERVAL 1 DAY,'-','') AS today
 """
 today = get_table_from_db(sql, db='aihems_api_db')
 year_date = today.today[0]
+
+#todo : byminute 테이블에 들어있는 device_id여야함
+#todo : model이 있을경우와 없을경우를 둘다 고려해야함
+#todo :
--- prediction_status_to_db_3_4.py
+++ prediction_status_to_db_3_4.py
@@ -1,67 +1,41 @@
 from utils import *
 
-# # 일단 모델을 로컬에 저장
-# member_name = input('member name : ')
-# appliance_name = input('appliance name : ')
-# # device_address = input('device_address:')
-#
-# device_address = search_device_address(member_name, appliance_name)
-
 query = """
-SELECT distinct(device_id)
+SELECT distinct(device_id), gateway_id
 FROM AH_USE_LOG_BYMINUTE_LABELED_copy
 """
-device_address_list = get_table_from_db(query, db='aihems_api_db')
-
-for device_address in device_address_list.device_id:
-    if device_address == '00158D0001A42F8A1':
+device_address_df = get_table_from_db(query, db='aihems_api_db')
+device_address_list = device_address_df.values.tolist()
+for device in device_address_list:
+    if device[0] == '00158D0001A42F8A1':
         continue
-    if device_address == '00158D000151B1F91':
-        continue
-    # if device_address == '00158D000151B2131':
-    #     continue
-    if device_address == '00158D000151B31C1':
-        continue
-    if device_address == '00158D000151B4441':
-        continue
-    if device_address == '00158D0001524AF71':
-        continue
-    if device_address == '00158D000151B4751':
-        continue
-    if device_address == '00158D0001A42F2F1':
-        continue
-    if device_address == '00158D0001A44BC51':
-        continue
-    if device_address == '00158D0001A459C11':
-        continue
-    # device_address = device_address[:-1]
-    # if device_address == '000D6F001257586D1':
-    #     continue
+    device_address = device[0]
+    gateway_id = device[1]
     gs = load('./sample_data/joblib/'+device_address+'_labeling.joblib')
-
+    test_bed_list = ['ep18270486', 'ep18270236', 'ep18270363']
     lag = 10
-
-    sql = f"""
-    SELECT AUL.*
-    FROM AH_GATEWAY AS AG
-    JOIN AH_USE_LOG_BYMINUTE_201903 AS AUL
-    ON AG.GATEWAY_ID=AUL.GATEWAY_ID
-    WHERE AUL.DEVICE_ID='{device_address}'
-    UNION
-    SELECT AUL.*
-    FROM AH_GATEWAY AS AG
-    JOIN AH_USE_LOG_BYMINUTE_201904 AS AUL
-    ON AG.GATEWAY_ID=AUL.GATEWAY_ID
-    WHERE AUL.DEVICE_ID='{device_address}'
-    """
-
-    # gateway_id = ""
-    # device_address_condition = f"AND LOG3.device_address = '{device_address}'"
-    # device_address_condition_1 = f"AND LOG4.device_address = '{device_address}'"
-    # gateway_id_condition = f"AND gateway_id = {gateway_id}"
-
-    # sql += device_address_condition
-    # sql += gateway_id_conditon
+    if gateway_id in test_bed_list:
+        sql = f"""
+        SELECT AUL.*
+        FROM AH_GATEWAY AS AG
+        JOIN AH_USE_LOG_BYMINUTE_201904 AS AUL
+        ON AG.GATEWAY_ID=AUL.GATEWAY_ID
+        WHERE AUL.DEVICE_ID='{device_address}'
+        """
+    else:
+        sql = f"""
+        SELECT AUL.*
+        FROM AH_GATEWAY AS AG
+        JOIN AH_USE_LOG_BYMINUTE_201903 AS AUL
+        ON AG.GATEWAY_ID=AUL.GATEWAY_ID
+        WHERE AUL.DEVICE_ID='{device_address}'
+        UNION
+        SELECT AUL.*
+        FROM AH_GATEWAY AS AG
+        JOIN AH_USE_LOG_BYMINUTE_201904 AS AUL
+        ON AG.GATEWAY_ID=AUL.GATEWAY_ID
+        WHERE AUL.DEVICE_ID='{device_address}'
+        """
 
     df = labeling_db_to_db(sql, db='aihems_api_db')
     if df.empty:
@@ -74,24 +48,31 @@ for device_address in device_address_list.device_id:
     df = df.iloc[:-lag]
 
     df['appliance_status'] = gs.predict(x)
-    # df.reset_index()
     df_dic = df.iloc[0].to_dict()
     key = df_dic['gateway_id']+'-'+df_dic['device_id']+'-'+df_dic['collect_date']+'-'+df_dic['collect_time']
-    # print(key)
-    sql = f"""
-        SELECT CONCAT(AUL.GATEWAY_ID,"-",AUL.DEVICE_ID,"-",AUL.COLLECT_DATE,"-",AUL.COLLECT_TIME) AS PRIMARY_KEY 
-        FROM AH_GATEWAY AS AG
-        JOIN AH_USE_LOG_BYMINUTE_LABELED_copy AS AUL
-        ON AG.GATEWAY_ID=AUL.GATEWAY_ID
-        WHERE AUL.DEVICE_ID='{device_address}'
-        AND COLLECT_DATE >= '20190301' 
-    """
+    if gateway_id in test_bed_list:
+        sql = f"""
+            SELECT CONCAT(AUL.GATEWAY_ID,"-",AUL.DEVICE_ID,"-",AUL.COLLECT_DATE,"-",AUL.COLLECT_TIME) AS PRIMARY_KEY 
+            FROM AH_GATEWAY AS AG
+            JOIN AH_USE_LOG_BYMINUTE_LABELED_copy AS AUL
+            ON AG.GATEWAY_ID=AUL.GATEWAY_ID
+            WHERE AUL.DEVICE_ID='{device_address}'
+            AND COLLECT_DATE >= '20190401' 
+                """
+    else:
+        sql = f"""
+            SELECT CONCAT(AUL.GATEWAY_ID,"-",AUL.DEVICE_ID,"-",AUL.COLLECT_DATE,"-",AUL.COLLECT_TIME) AS PRIMARY_KEY 
+            FROM AH_GATEWAY AS AG
+            JOIN AH_USE_LOG_BYMINUTE_LABELED_copy AS AUL
+            ON AG.GATEWAY_ID=AUL.GATEWAY_ID
+            WHERE AUL.DEVICE_ID='{device_address}'
+            AND COLLECT_DATE >= '20190301' 
+        """
     primary_key_df = get_table_from_db(sql, db='aihems_api_db')
     if primary_key_df.empty:
         primary_key = " "
     else:
         primary_key = primary_key_df.primary_key[0]
-    # print(primary_key)
     if key == primary_key:
         print("duplicate : "+ device_address)
     else:
