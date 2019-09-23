a74e4fc738a8c09c09881ce5e299dbb3cdbb488d
CHAEUN PARK <codjs0910@naver.com>
2019-09-23 15:57:36 +0900

    #CU silvercare Labeling Input GetJson

--- silvercare_api.py
+++ silvercare_api.py
@@ -7,68 +7,6 @@ from joblib import load, dump
 class SilverCare_Labeling(Resource):
     def post(self):
         try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('gateway_id', type=str)
-            parser.add_argument('collect_date', type=str)
-            parser.add_argument('collect_time', type=str)
-            args = parser.parse_args()
-
-            gateway_id = args['gateway_id']
-            collect_date = args['collect_date']
-            collect_time = args['collect_time']
-
-            gateway_list = ['ep18270401', 'ep18270403', 'ep18270414', 'ep18270224']
-
-            if gateway_id == 'ep18270192':
-                model_name = 'silvercare_model_2'
-            elif gateway_id in gateway_list:
-                model_name = 'silvercare_model_1'
-            else:
-                model_name = 'silvercare_model'
-
-            cur_time = collect_date + collect_time
-
-            sql = f"""
-                SELECT *
-                FROM AH_USE_LOG_BYMINUTE
-                WHERE 1=1
-                    AND GATEWAY_ID = '{gateway_id}'
-                    AND CONCAT( COLLECT_DATE, COLLECT_TIME) > DATE_FORMAT( DATE_ADD( STR_TO_DATE( '{cur_time}', '%Y%m%d%H%i'), INTERVAL -10 MINUTE), '%Y%m%d%H%i')
-                ORDER BY COLLECT_DATE, COLLECT_TIME
-                LIMIT 0, 10 
-            """
-
-            df = utils.get_table_from_db(sql)
-
-            x, y = utils.split_x_y(df, x_col='energy_diff')
-            x = [[i for i in x]]
-
-            model = load(f'./sample_data/joblib/silvercare/{model_name}.joblib')
-            y = model.predict(x)
-            y = str(y[0])
-
-            return {'flag_success': True, 'predicted_status': y}
-        except Exception as e:
-            return {'flag_success': False, 'error': str(e)}
-
-
-class SilverCare_test(Resource):
-    def post(self):
-        try:
-            # parser = reqparse.RequestParser()
-            # parser.add_argument('gateway_id', type=str)
-            # parser.add_argument('device_id', type=str)
-            # parser.add_argument('collect_date', type=str)
-            # parser.add_argument('collect_time', type=str)
-            # parser.add_argument('data', default=[], type=list, action="append", location='json')
-            # args = parser.parse_args()
-            #
-            # gateway_id = args['gateway_id']
-            # device_id = args['device_id']
-            # collect_date = args['collect_date']
-            # collect_time = args['collect_time']
-            # data = args['data']
-
             json_data = request.get_json(silent=True, cache=False)
             gateway_id = json_data['gateway_id']
             device_id = json_data['device_id']
@@ -94,4 +32,6 @@ class SilverCare_test(Resource):
             return {'flag_success': True, 'predicted_status': y}
         except Exception as e:
             return {'flag_success': False, 'error': str(e)}
+
+
 
