c6904b76fb3abd0fee1868041f8ef8c3307bb1c3
shin <byungjin0826@gmail.com>
2019-12-27 17:43:12 +0900

    api new

--- api_2.py
+++ api_2.py
@@ -117,5 +117,5 @@ def hello():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='127.0.0.1', debug=True)
