6a7c49749069a0b6a0173020a440214e8de37a36
shin <byungjin0826@gmail.com>
2019-12-27 11:11:37 +0900

    포트 수정

--- api.py
+++ api.py
@@ -584,7 +584,7 @@ api.add_resource(Make_Model_Status, '/make_model_status')
 
 
 if __name__ == '__main__':
-    # app.run(host = '0.0.0.0', port=5000, debug=True)
+    app.run(host = '0.0.0.0', port=5000, debug=True)
     app.run(debug=True)
     # app.run(host='127.0.0.1', port=5000, debug=True)
     # print('a')
