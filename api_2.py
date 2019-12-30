6b239a980a556d3a1466d05f8ca24730f0afd1d6
shin <byungjin0826@gmail.com>
2019-12-30 11:21:47 +0900

    json 오류 수정

--- api_2.py
+++ api_2.py
@@ -36,10 +36,10 @@ def labeling_by_device_for_one_day():
 
         y = dl.labeling(device_id=device_id, gateway_id=gateway_id, collect_date=collect_date)
         print(y)
-        return {'flag_success': True, 'predicted_status': y}
+        return jsonify({'flag_success': True, 'predicted_status': y})
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/schedule/', methods=['GET', 'POST'])
@@ -52,12 +52,12 @@ def ai_schedule():
         df.columns = ['dayofweek', 'time', 'end', 'duration', 'appliance_status']
         result = df.loc[:, ['dayofweek', 'time', 'appliance_status']].to_dict('index')
 
-        return {'flag_success': True
+        return jsonify({'flag_success': True
                 , 'device_id': device_id
-                , 'result': result}
+                , 'result': result})
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/cbl_info/', methods=['GET', 'POST'])
@@ -68,10 +68,10 @@ def cbl_info():
 
         cbl, reduction_energy = dr.cbl_info(house_no=house_no, request_dr_no=request_dr_no)
 
-        return {'flag_success': True, 'cbl': cbl, 'reduction_energy': reduction_energy}
+        return jsonify({'flag_success': True, 'cbl': cbl, 'reduction_energy': reduction_energy})
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/dr_recommendation/', methods=['GET', 'POST'])  # todo: dr 추천 작업 중
@@ -83,12 +83,10 @@ def dr_recommendation():
         cbl, reduction_energy = dr.cbl_info(house_no=house_no, request_dr_no=request_dr_no)
         dr_type, duration = dl.get_dr_info(request_dr_no=request_dr_no)
 
-
-
         return 0
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/make_model_elec/', methods=['GET', 'POST'])
@@ -96,10 +94,10 @@ def make_model_elec():
     try:
         house_no = request.json['house_no']
         score = mt.make_model_elec(house_no=house_no)
-        return {'flag_success': True, 'best_score': str(score)}
+        return jsonify({'flag_success': True, 'best_score': str(score)})
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/make_model_status/', methods=['GET', 'POST'])
@@ -107,10 +105,10 @@ def make_model_status():
     try:
         device_id = request.json['device_id']
         dump_path, best_score = mt.make_model_status(device_id=device_id)
-        return {'flag_success': True, 'dump_path': str(dump_path), 'score': str(best_score)}
+        return jsonify({'flag_success': True, 'dump_path': str(dump_path), 'score': str(best_score)})
 
     except Exception as e:
-        return {'flag_success': False, 'error': str(e)}
+        return jsonify({'flag_success': False, 'error': str(e)})
 
 
 @app.route('/test/', methods=['GET', 'POST'])
